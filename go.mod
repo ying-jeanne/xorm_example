54e63afda778bc142c683f130aed0e4b2842aea6
Ying WANG <ying.wang@grafana.com>
2022-07-12 17:57:22 -0500

    modify the code to get cpu/memory profiling

---
+++ go.mod
@@ -0,0 +1,16 @@
+module xorm_example
+
+go 1.17
+
+require (
+	github.com/goccy/go-json v0.8.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mattn/go-sqlite3 v1.14.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pkg/profile v1.6.0 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	xorm.io/builder v0.3.11-0.20220531020008-1bd24a7dc978 // indirect
+	xorm.io/xorm v1.3.1 // indirect
+)
--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/pkg/profile"
 	"xorm.io/xorm"
 	"xorm.io/xorm/names"
 )
+
+const numrecords = 2000
 
 type Team struct {
 	ID        int       `xorm:"'id' pk autoincr"`
@@ -19,81 +23,95 @@ type Team struct {
 	Email     string
 }
 
-type Team2 struct {
-	ID   int    `xorm:"'id' pk autoincr"`
-	Name string `xorm:"name"`
-	// OrgID     int  when we are trying to not put tag, xorm is actually error out for get
-	OrgID     int       `xorm:"org_id"`
-	CreatedAt time.Time `xorm:"'created'"`
-	UpdatedAt time.Time `xorm:"'updated'"`
-	Email     string
-}
-
-func (t Team2) TableName() string {
-	return "team"
-}
-
 func insertTeam(e *xorm.Engine, team1 Team) error {
 	// for insert, xorm is actually checking that all the filled field has a corresponding column name, if not, error out
 	// not set field would be filled directly with default value when creation
-	var err error
-	if _, err = e.Insert(&team1); err != nil {
-		log.Fatal(err)
-	}
+	_, err := e.Insert(&team1)
 	return err
 }
 
-func getTeam(e *xorm.Engine, name string) Team {
-	teams := []Team{}
-	err := e.Find(&teams)
-	if err != nil {
-		log.Fatal(err)
-	}
+func getTeam(e *xorm.Engine, name string) (bool, Team, error) {
 	var team1 Team
-	_, err = e.Where("name=?", name).Get(&team1)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return team1
+	found, err := e.Where("name=?", name).Get(&team1)
+	return found, team1, err
 }
 
-func deleteTeam(e *xorm.Engine, name string) {
-	_, err := e.Exec("DELETE FROM team WHERE name=?", name)
-	if err != nil {
-		log.Fatal(err)
-	}
+func deleteTeam(e *xorm.Engine, name string) error {
+	_, err := e.Exec("DELETE FROM team WHERE name=? Returning id", name)
+	return err
 }
 
-func updateTeam(e *xorm.Engine, team Team) (int64, error) {
-	affected, err := e.ID(team.ID).Update(team)
-	return affected, err
+func updateTeam(e *xorm.Engine, team Team) error {
+	_, err := e.ID(team.ID).Update(team)
+	return err
 }
 
 func main() {
+	// set engine of sqlite3 here
 	engine, err := xorm.NewEngine("sqlite3", "grafana.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	engine.SetTableMapper(names.GonicMapper{})
-	team1 := Team{Name: "myname4", OrgID: 1, CreatedAt: time.Now(), UpdatedAt: time.Now()}
-	err = insertTeam(engine, team1)
-	if err != nil {
-		log.Fatal(err)
+
+	// The test senarios:
+	// 1. Create 1000 teams with go struct
+	// 2. Verify after each creation so 1000 get with go struct casting
+	// 3. Update 1000 teams with only name field
+	// 4. Verify after update so 1000 get with go struct casting
+	// 5. Delete 1000 teams with name field, return the id deleted
+	// 6. Get 1000 teams which is nolonger existing
+
+	// start timer
+	start := time.Now()
+
+	// CPU profiling by default related doc here: https://flaviocopes.com/golang-profiling/
+	// defer profile.Start().Stop()
+
+	// Memory profiling
+	defer profile.Start(profile.MemProfile).Stop()
+
+	for i := 0; i < numrecords; i++ {
+		num := strconv.Itoa(i)
+		team1 := Team{Name: "mynamee" + num, OrgID: 1, CreatedAt: time.Now(), UpdatedAt: time.Now()}
+		err = insertTeam(engine, team1)
+		// fmt.Printf("the inserted team %+v\n", team1)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, team3, err := getTeam(engine, team1.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// fmt.Printf("the get team %+v\n", team3)
+
+		// here it is very confusing, since xorm omit the default value OrgID 0 here, we would have to
+		// force the update by calling .AllCols().Update or .Cols("org_id").Update
+		// but if we put .AllCols() we have to put all the fields that are mandatory so it is also not convinient
+		team2 := Team{ID: team3.ID, OrgID: 0, Name: "princess" + num}
+		err = updateTeam(engine, team2)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, team4, err := getTeam(engine, team2.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// fmt.Printf("the updated team %+v\n", team4)
+
+		err = deleteTeam(engine, team4.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, _, err = getTeam(engine, team4.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// fmt.Printf("the deleted team %+v\n", f)
 	}
 
-	team3 := getTeam(engine, team1.Name)
-	fmt.Printf("the team3 is %v \n", team3)
-
-	// here it is very confusing, since xorm omit the default value OrgID 0 here, we would have to
-	// force the update by calling .AllCols().Update or .Cols("org_id").Update
-	// but if we put .AllCols() we have to put all the fields that are mandatory so it is also not convinient
-	team2 := Team{ID: team3.ID, OrgID: 0, Name: "princess"}
-	_, err = updateTeam(engine, team2)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	team4 := getTeam(engine, team2.Name)
-	fmt.Printf("the team4 is %v after update \n", team4)
-	deleteTeam(engine, team2.Name)
+	fmt.Println("the result is: ", time.Since(start))
 }
